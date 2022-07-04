9ca852fc2dfad5b459af40391eb45ee3306efe47
9xKiritix6 <horbyramos.rh@gmail.com>
2022-07-05 02:04:12 +0800

    progress!

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Script for Windows 10/11 Devices that Includes:
 - Disabling Windows Updates
 - Disk Defragmentation/Trimming
 - Disk Cleanup
+- Debloating
--- mainSequence.cmd
+++ mainSequence.cmd
@@ -2,6 +2,9 @@
 
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "Startup Scripts" /min "mainSequence.cmd" %* && exit
 
-call disableWindowsUpdates.bat
-call diskCleanUp.bat
-call diskDefragmentationAndTrimming.bat
+call modules\firstLaunchRequirements.bat
+call modules\disableWindowsUpdates.bat
+call modules\debloating.bat
+
+call modules\diskCleanUp.bat
+call modules\diskDefragmentationAndTrimming.bat
